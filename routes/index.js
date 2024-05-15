52a8ed863db48cacd5cee2d8b86529bbfc1e8f41
Zala <zala.developer@gmail.com>
2024-05-15 20:04:52 +0930

    add authentication middleware create route for authentication

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const passport = require("passport");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("auth/login.ejs", { title: "Task Tracker" });
+});
+
+// Google OAuth login route
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+// Google OAuth callback route
+router.get(
+  "/oauth2callback",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+//OAuth logout router
+router.get("/logout", function (req, res) {
+  req.logout(function () {
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
