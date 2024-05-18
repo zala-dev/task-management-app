a5e24062f8c91f1e071a5b6d729629c3d43967cf
Zala <zala.developer@gmail.com>
2024-05-18 21:53:07 +0930

    update project router

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const projectCtrl = require("../controllers/projectController");
 const ensuredLoggedIn = require("../config/ensureLoggedIn");
 
+router.get("/", ensuredLoggedIn, projectCtrl.index);
+
 router.get("/new", ensuredLoggedIn, projectCtrl.newProject);
 
 router.post("/", ensuredLoggedIn, projectCtrl.create);
