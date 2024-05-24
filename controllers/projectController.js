e947050f158e26faa9c7180c798ec66c23e85089
Zala <zala.developer@gmail.com>
2024-05-24 20:46:44 +0930

    add code comments and formating

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -10,6 +10,7 @@ module.exports = {
   toggleIsComplete,
 };
 
+// function to retrieve and render projects for the logged-in user
 async function index(req, res) {
   try {
     const userId = req.user._id;
@@ -23,11 +24,7 @@ async function index(req, res) {
     const startIndex = (page - 1) * perPage;
     const endIndex = Math.min(startIndex + perPage, totalProjects);
 
-    console.log("Page:", page);
-    console.log("Total Projects:", totalProjects);
-    console.log("Start Index:", startIndex);
-    console.log("End Index:", endIndex);
-
+    // Render empty project list if no projects found
     if (totalProjects === 0 || startIndex >= totalProjects) {
       return res.render("projects/projectList", {
         title: "My Projects",
@@ -39,12 +36,12 @@ async function index(req, res) {
       });
     }
 
+    // Fetch projects for the current page
     const projects = await Project.find({ user: userId })
       .skip(startIndex)
       .limit(perPage);
 
-    console.log("Fetched Projects:", projects);
-
+    // Render project list
     res.render("projects/projectList", {
       title: "My Projects",
       projects,
@@ -54,13 +51,15 @@ async function index(req, res) {
       endIndex,
     });
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Error fetching data: " + err.message);
+    res.status(500).send(err.message);
   }
 }
 
+// function to create a new project
 async function create(req, res) {
+  // conver user ID to string
   req.body.user = req.user.id.toString();
+  // remove empty fields from request body
   for (let key in req.body) {
     if (req.body[key] === "") delete req.body[key];
   }
@@ -75,11 +74,10 @@ async function create(req, res) {
   };
 
   try {
+    // Create new project
     await Project.create(newProject);
-    console.log("Project created... Redirecting to dashbaord...");
     res.redirect("/projects");
   } catch (err) {
-    console.log("Error creating project: ", err.message);
     res.render("projects/createProject", {
       title: "Add Project",
       errorMsg: err.message,
@@ -87,16 +85,22 @@ async function create(req, res) {
   }
 }
 
+// funnction to render the form for adding a new project
 async function newProject(req, res) {
+  // Render new project form
   res.render("projects/createProject", { title: "Add Project", errorMsg: "" });
 }
 
+// function to delete a project
 async function deleteProject(req, res) {
+  // Delete project by ID
   await Project.deleteOne({ _id: req.params.id });
   res.redirect("/projects");
 }
 
+// function to render the form for editing a project
 async function editProject(req, res) {
+  // Find project by ID and render edit form
   const project = await Project.findOne({ _id: req.params.id });
   res.render("projects/editProject", {
     project,
@@ -104,27 +108,32 @@ async function editProject(req, res) {
   });
 }
 
+// function to update the project details
 async function updateProject(req, res) {
+  // Update project details
   await Project.updateOne({ _id: req.params.id }, req.body);
   res.redirect("/projects");
 }
 
+// function to toggle project completion status
 async function toggleIsComplete(req, res) {
   try {
     const projectId = req.params.id;
     const project = await Project.findById(projectId);
 
+    // Check if project exists
     if (!project) {
-      return res.status(404).json({ error: "Project not found" });
+      return res.status(404).send("Project not found");
     }
 
+    // Toggle completion status
     project.isComplete = !project.isComplete;
 
+    // Save changes
     await project.save();
 
     res.redirect("/projects");
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 }
